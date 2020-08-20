247ccf23b615a5c4618be55d8c62104b3e9f3a60
Aaron Sutula <hi@asutula.com>
2020-08-19 19:07:49 -0500

    add SwiftGRPC target dep
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "GRPCTextile",
-            dependencies: []),
+            dependencies: ["SwiftGRPC"]),
     ]
 )
